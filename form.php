1fbba23837bdcafaf088dba6debc9f73b161ca3c
Mehmet Açıkgöz <102829820+mhmtackgz@users.noreply.github.com>
2022-11-17 23:55:55 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="textbox.css">
+</head>
+<body style="text-align: center">
+    <div class="textbox">
+    <form method="post" action="ekle.php" style="text-align: center">
+  <label  for="fname">Adınız Soyadınız:</label><br>
+  <input type="text"  name="adsoyad"><br><br>
+  <label for="lname">Telefon Numaranız:</label><br>
+  <input type="text"  name="telefonno"><br><br>
+  <div class=textbox>
+  <input type="submit" name = "buton" value="EKLE">
+  </div>
+  <div>
+    <br>
+    <button>
+        <a href="listele.php">KAYITLARI GÖSTER</a>
+    </button>
+  </div>
+ 
+</form>
+
+    </div>
+</body>
+</html>
+
+
